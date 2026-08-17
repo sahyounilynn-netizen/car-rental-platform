6ca05ed78226e15358937bca990a72ea4072f750
Lynn Sahyouni <Sahyounilynn@gmail.com>
2026-08-17 22:08:24 +0300

    Configure Express backend for Vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import { logger } from "./lib/logger";
 
 const app = createApp();
 
-app.listen(env.PORT, () => {
-  logger.info(`Server listening on http://localhost:${env.PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(env.PORT, () => {
+    logger.info(`Server listening on http://localhost:${env.PORT}`);
+  });
+}
+
+export default app;
